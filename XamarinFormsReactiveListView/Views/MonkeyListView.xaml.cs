f0f070d80cf3bc27ec550768b40aaaf19f0b25b2
Brad Browne <brad@mapdojo.com>
2015-05-19 17:23:22 +1000

    Got SelectedItem working.

--- XamarinFormsReactiveListView/Views/MonkeyListView.xaml.cs
+++ XamarinFormsReactiveListView/Views/MonkeyListView.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms;
 using ReactiveUI;
 using System.Reactive;
 using XamarinFormsReactiveListView.ViewModels;
+using System.Diagnostics;
 
 namespace XamarinFormsReactiveListView.Views
 {
@@ -16,7 +17,17 @@ namespace XamarinFormsReactiveListView.Views
 
 			this.OneWayBind(ViewModel, x => x.Monkeys, x => x.MonkeyList.ItemsSource);
 			this.BindCommand(ViewModel, vm => vm.AddMonkey, v => v.AddMonkey);
+
+			var itemSelected = this.WhenAny(x => x.MonkeyList.SelectedItem, x => x.Value != null);
+			Select = ReactiveCommand.CreateAsyncTask (itemSelected, async x => {
+				MonkeyList.SelectedItem = null;
+				Debug.WriteLine("ItemSelected");
+			});
+			this.WhenAnyValue(x => x.MonkeyList.SelectedItem)
+				.InvokeCommand(this, x => x.Select);
 		}
+
+		public ReactiveCommand<Unit> Select { get; protected set; }
 
 		public MonkeyListViewModel ViewModel {
 			get { return (MonkeyListViewModel)GetValue(ViewModelProperty); }
