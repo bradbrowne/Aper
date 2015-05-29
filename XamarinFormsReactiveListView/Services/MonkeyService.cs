0fecdfc2dc1c3654f31288c00486464420f17f64
Brad Browne <brad@mapdojo.com>
2015-05-29 10:59:43 +1000

    Added AsyncLock to SQLite operations.

--- XamarinFormsReactiveListView/Services/MonkeyService.cs
+++ XamarinFormsReactiveListView/Services/MonkeyService.cs
@@ -16,26 +16,50 @@ namespace XamarinFormsReactiveListView
 {
 	public class MonkeyService : IMonkeyService
 	{
+		private static readonly AsyncLock Mutex = new AsyncLock ();
 		SQLiteAsyncConnection databaseAsync;
+
+		public async Task CreateDatabaseAsync ()
+		{
+			using (await Mutex.LockAsync ().ConfigureAwait (false)) {
+				await databaseAsync.CreateTableAsync<Monkey> ().ConfigureAwait (false);
+			}
+		}
 
 		public async Task<List<Monkey>> GetAllAsync ()
 		{
-			return await databaseAsync.Table<Monkey> ().ToListAsync();
+			List<Monkey> monkeys;
+			using (await Mutex.LockAsync ().ConfigureAwait (false)) {
+				monkeys = await databaseAsync.Table<Monkey> ().ToListAsync ().ConfigureAwait (false);
+			}
+			return monkeys;
 		}
 
 		public async Task<int> DeleteAsync(Monkey monkey)
 		{
-			return await databaseAsync.DeleteAsync<Monkey>(monkey.Id);
+			int result;
+			using (await Mutex.LockAsync ().ConfigureAwait (false)) {
+				result = await databaseAsync.DeleteAsync<Monkey>(monkey.Id).ConfigureAwait (false);
+			}
+			return result;
 		}
 
 		public async Task<int> InsertAsync(Monkey monkey)
 		{
-			return await databaseAsync.InsertAsync(monkey);
+			int result;
+			using (await Mutex.LockAsync ().ConfigureAwait (false)) {
+				result = await databaseAsync.InsertAsync(monkey).ConfigureAwait (false);
+			}
+			return result;
 		}
 
 		public async Task<int> UpdateAsync(Monkey monkey)
 		{
-			return await databaseAsync.UpdateAsync(monkey);
+			int result;
+			using (await Mutex.LockAsync ().ConfigureAwait (false)) {
+				result = await databaseAsync.UpdateAsync(monkey).ConfigureAwait(false);
+			}
+			return result;
 		}
 
 		public MonkeyService ()
